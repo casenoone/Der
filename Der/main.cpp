2fe8759b8f0dd18e463bad99e9b4972a2b5b8cd7
casenoone <787502862@qq.com>
2020-09-11 11:06:34 +0800

    1

--- Der/main.cpp
+++ Der/main.cpp
@@ -295,7 +295,10 @@ void midpointLine(int x0,int y0,int x1,int y1, TGAImage &image, TGAColor color){
 
 }
 
+
+
 //Bresenham’s Line Drawing Algorithm
+
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
 
 	bool steep = false;
@@ -304,7 +307,6 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
 		std::swap(x0, y0);
 		std::swap(x1, y1);
 		steep = true;
-
 	}
 
 	if (x0>x1) {
@@ -327,6 +329,7 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
 			image.set(y, x, TGAColor(color));
 
 		}
+
 		else {
 
 			image.set(x, y, TGAColor(color));
@@ -341,28 +344,119 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
 			error2 -= dx * 2;
 
 		}
+
+	}
+
+}
+
+void swap_vec3(point3 &A,point3 &B) {//vec3里实现一个复制构造函数
+	point3 temp;
+	temp = A;
+	A = B;
+	B = temp;
+}
+
+void trianle_box(point3 &A, point3 &B, point3 &C) {
+
+}
+
+//三角形包围盒顶点坐标：（x取最小的x，y取最小的y），（x取最大的x，y取最大的y）
+//扫描填充法：首先求三角形包围盒，然后遍历包围盒覆盖的每一个像素，利用叉积判断该像素是否在三角形内
+//具体实现不写了
+void scan_triangle(point3 &A,point3 &B,point3 &C, TGAImage &image, TGAColor color) {
+	
+}
+
+//重心坐标法填充三角形
+//同样需要计算包围盒
+/*bgra[0] = B;
+bgra[1] = G;
+bgra[2] = R;
+bgra[3] = A;*/
+
+void barycentric_triangle(point3 &A, point3 &B, point3 &C, TGAImage &image, vec3 colorA,vec3 colorB,vec3 colorC) {
+
+	vec3 tempcolor;
+	point3 minP = A;
+	point3 maxP = A;
+	if (minP.x() > B.x()) {
+		minP.e[0] = B.x();
+	}
+	if (minP.x() > C.x()) {
+		minP.e[0] = C.x();
+	}
+
+	if (maxP.x() < B.x()) {
+		maxP.e[0] = B.x();
+	}
+	if (maxP.x() < C.x()) {
+		maxP.e[0] = C.x();
+	}
+
+	if (minP.y() > B.y()) {
+		minP.e[1] = B.y();
+	}
+	if (minP.y() > C.y()) {
+		minP.e[1] = C.y();
+	}
+
+	if (maxP.y() < B.y()) {
+		maxP.e[1] = B.y();
+	}
+	if (maxP.y() < C.y()) {
+		maxP.e[1] = C.y();
+	}
+	
+	double x_min = floor(minP.x());
+	double x_max = ceil(maxP.x());
+	double y_min = floor(minP.y());
+	double y_max = ceil(maxP.y());
+
+
+
+	double f_alpha = (B.y() - C.y())*A.x() + (C.x() - B.x())*A.y() + B.x()*C.y() - C.x()*B.y();
+	//double f_beta = (C.y() - A.y())*B.x() + (A.x() - C.x())*B.y() + C.x()*A.y() - A.x()*C.y();
+	double f_beta = (A.y() - C.y())*B.x() + (C.x() - A.x())*B.y() + A.x()*C.y() - C.x()*A.y();
+	double f_gamma = (A.y() - B.y())*C.x() + (B.x() - A.x())*C.y() + A.x()*B.y() - B.x()*A.y();
+
+
+	double f12= (B.y() - C.y())*(-1) + (C.x() - B.x())*(-1) + B.x()*C.y() - C.x()*B.y();
+	//double f20= (C.y() - A.y())*(-1) + (A.x() - C.x())*(-1) + C.x()*A.y() - A.x()*C.y();
+	double f20 = (A.y() - C.y())*(-1) + (C.x() - A.x())*(-1) + A.x()*C.y() - C.x()*A.y();
+	double f01= (A.y() - B.y())*(-1) + (B.x() - A.x())*(-1) + A.x()*B.y() - B.x()*A.y();
+
+	for (double y = y_min; y <= y_max; y++) {
+		
+		for (double x = x_min; x <= x_max; x++) {
+			double alpha= (B.y() - C.y())*x + (C.x() - B.x())*y + B.x()*C.y() - C.x()*B.y();
+			//double beta = (C.y() - A.y())*x + (A.x() - C.x())*y + C.x()*A.y() - A.x()*C.y();
+			double beta = (A.y() - C.y())*x + (C.x() - A.x())*y + A.x()*C.y() - C.x()*A.y();
+
+			double gamma= (A.y() - B.y())*x + (B.x() - A.x())*y + A.x()*B.y() - B.x()*A.y();
+
+
+			if (alpha >= 0 && beta >= 0 && gamma >= 0) {
+				std::cout << 1 << std::endl;
+
+				if ((alpha>0||f_alpha*f12>0)&&(beta>0||f_beta*f20>0)&&(gamma>0||f_gamma*f01>0)) {
+					//计算颜色插值
+
+					tempcolor = alpha*colorA + beta*colorB + gamma*colorC;
+					TGAColor lastcolor = TGAColor(tempcolor.x(), tempcolor.y(), tempcolor.z(), 255);
+					image.set(x, y, lastcolor);
+
+				}
+			}
+		}
 	}
 }
 
-
 int main(int argc, char** argv) {
 
-	TGAImage image(100, 100, TGAImage::RGB);
-	for (int i = 0; i<1; i++) {
-
-		//line(13, 13, 13, 1, image, white);
-		/*line(20, 13, 40, 80, image, red);
-		line(80, 40, 13, 20, image, red);*/
-		
-		/*midpointLine(1, 1, 30, 2, image, red);
-		midpointLine(1, 1, 2, 30, image, red);
-		midpointLine(1, 1, 100, 0, image, red);*/
-		/*drawTriangle(point3(12, 9, 0), point3(24, 15, 0), point3(6, 21, 0), image, red);
-		drawTriangle(point3(12, 9, 0), point3(24, 15, 0), point3(15, 6, 0), image, white);*/
-		drawTriangle(point3(10, 10, 0), point3(0, 0, 0), point3(10, 0, 0), image, red);
-		drawTriangle(point3(10, 10, 0), point3(10, 0, 0), point3(20, 0, 0), image, white);
-
-	}
+	TGAImage image(1000, 1000, TGAImage::RGB);
+	
+	midpointLine(0, 0, 1000, 1000, image, red);
+	barycentric_triangle(point3(0, 0, 0), point3(10, 10, 0), point3(20, 0, 0), image, vec3(255, 0, 0), vec3(255, 0, 0), vec3(255, 0, 0));
 
 	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
 	image.write_tga_file("output.tga");
